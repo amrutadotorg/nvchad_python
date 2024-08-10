a90afabd47904cea5eb915b70b92304ce460f68f
Amruta.org <38246728+amrutadotorg@users.noreply.github.com>
2024-08-10 08:38:25 +0200

    Create options.lua

---
+++ options.lua
@@ -0,0 +1,13 @@
+require "nvchad.options"
+-- https://neovim.io/doc/user/provider.html#clipboard-osc52
+vim.g.clipboard = {
+  name = 'OSC 52',
+  copy = {
+    ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+  },
+  paste = {
+    ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+  },
+}
