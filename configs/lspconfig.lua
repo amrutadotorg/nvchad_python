24eca4ecc72167a7adc1a397a34af9c63fc4e27e
Amruta.org <38246728+amrutadotorg@users.noreply.github.com>
2024-08-10 19:16:39 +0200

    Update lspconfig.lua

--- configs/lspconfig.lua
+++ configs/lspconfig.lua
@@ -1,9 +1,7 @@
--- load defaults i.e lua_lsp
 require("nvchad.configs.lspconfig").defaults()
 
 local lspconfig = require "lspconfig"
 
--- EXAMPLE
 local servers = { "pyright","ruff_lsp" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
@@ -17,9 +15,3 @@ for _, lsp in ipairs(servers) do
   }
 end
 
--- configuring single server, example: typescript
--- lspconfig.tsserver.setup {
---   on_attach = nvlsp.on_attach,
---   on_init = nvlsp.on_init,
---   capabilities = nvlsp.capabilities,
--- }
