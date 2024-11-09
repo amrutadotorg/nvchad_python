faa18f5a72191ed7d1223d350801c573eeaea8c6
Amruta.org <38246728+amrutadotorg@users.noreply.github.com>
2024-11-09 18:50:24 +0100

    Update lspconfig.lua ruff part

--- configs/lspconfig.lua
+++ configs/lspconfig.lua
@@ -2,7 +2,7 @@ require("nvchad.configs.lspconfig").defaults()
 
 local lspconfig = require "lspconfig"
 
-local servers = { "pyright","ruff_lsp" }
+local servers = { "pyright","ruff" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
