eeba33c9ecaae7fc81a1101143cf7c4f180b84e6
Amruta.org <38246728+amrutadotorg@users.noreply.github.com>
2024-12-23 17:06:37 +0100

    Update options.lua add cursor position

--- options.lua
+++ options.lua
@@ -11,3 +11,14 @@ vim.g.clipboard = {
     ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
   },
 }
+
+vim.api.nvim_create_autocmd("BufReadPost", {
+  callback = function()
+    local mark = vim.api.nvim_buf_get_mark(0, '"')
+    local lnum = mark[1]
+    local col = mark[2]
+    if lnum > 0 and lnum <= vim.api.nvim_buf_line_count(0) then
+      vim.api.nvim_win_set_cursor(0, { lnum, col })
+    end
+  end,
+})
